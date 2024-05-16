fca169d0a257b7af5cbdbf9388f5184c42c80bc6
Bakhar Ilya <artemgray22@gmail.com>
2024-05-16 22:28:05 +0300

    First commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+use App\Repository\UserRepository;
+
+class UserController extends AbstractController
+{
+    public function __construct(
+        private readonly UserRepository $userRepository,
+    ) {}
+
+    #[Route('/user', name: 'app_user', methods: ['GET'])    ]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        return $this->render('user/index.html.twig', [
+           'users' => $this->userRepository->findAll(),
+        ]);
+    }
+}
